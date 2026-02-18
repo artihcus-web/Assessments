f225bb77846e8aa7ea8df95bc86494d2019f0992
Ganesh <dhanasriganesh9@gmail.com>
2026-02-18 12:10:29 +0530

    log

--- src/components/routers/Routers.jsx
+++ src/components/routers/Routers.jsx
@@ -3,15 +3,17 @@ import Home from '../pages/Home'
 import Products from '../pages/Products'
 import Contact from '../pages/Contact'
 import Cart from '../pages/Cart'
+import Login from '../pages/Login'
 import { Routes, Route } from 'react-router-dom'
 function Routers() {
   return (
     <div>
         <Routes>
-            <Route path='/' element={<Home />} />
+            <Route path='/assessments-dashboard' element={<Home />} />
             <Route path='/products' element={<Products />} />
             <Route path='/contact' element={<Contact />} />
             <Route path='/cart' element={<Cart />} />
+            <Route path='/' element={<Login />} />
         </Routes>
     </div>
   )
